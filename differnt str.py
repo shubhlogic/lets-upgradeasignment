90fe9a18b0e6c9d1a56001e9f686fee1909767a3
shubhlogic <59937569+shubhlogic@users.noreply.github.com>
2020-12-11 18:32:24 +0530

    Five different string operation

---
+++ differnt str.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 5 different function in string python
+
+# In[13]:
+
+
+str='Shubham Shewale'
+
+
+# In[14]:
+
+
+print(str.replace('Shubham','hi'))
+
+
+# #String Reverse
+
+# In[15]:
+
+
+abc='datascience'
+
+
+# In[17]:
+
+
+''.join(reversed(abc))
+
+
+# String Join
+
+# In[21]:
+
+
+s1='I'
+s2='Python'
+
+
+# In[22]:
+
+
+s1.join(s2)
+
+
+# Split
+
+# In[23]:
+
+
+s1='I love my country'
+
+
+# In[24]:
+
+
+s1.split()
+
+
+# In[25]:
+
+
+#compare
+
+
+# In[26]:
+
+
+s1='python'
+s2='python'
+if s1==s2:
+    print('String same')
+
+
+# lowercase and uppercase
+
+# In[27]:
+
+
+s1.upper()
+
+
+# In[28]:
+
+
+s2.lower()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
